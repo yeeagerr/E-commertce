2d74cb6d20624a3e8adfd5104bcea7c23ede1701
Habib <heandsome083@gmail.com>
2023-11-08 19:39:14 +0700

    Fix categori section

--- scripts/animateJs/sliderClick.js
+++ scripts/animateJs/sliderClick.js
@@ -31,3 +31,27 @@ rightBtn.addEventListener("click", () => {
 });
 
 clickSlider(Counter);
+
+function slideKategori() {
+  const alert = document.querySelector(".alert-danger");
+  const leftBtn = document.querySelector(".left-kategori");
+  const rightBtn = document.querySelector(".right-kategori");
+
+  leftBtn.addEventListener("click", () => {
+    alert.style.display = "flex";
+
+    setTimeout(() => {
+      location.reload();
+    }, 1500);
+  });
+
+  rightBtn.addEventListener("click", () => {
+    alert.style.display = "flex";
+
+    setTimeout(() => {
+      location.reload();
+    }, 1500);
+  });
+}
+
+slideKategori();
